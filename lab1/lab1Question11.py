41c3656504eba77bcd9e0fb9ab5443544388661e
roketson <71538179+roketson@users.noreply.github.com>
2020-09-18 22:54:54 -0500

    Update lab1Question11.py
    added liquid density to drag and added drag to the accel equation

--- lab1/lab1Question11.py
+++ lab1/lab1Question11.py
@@ -16,7 +16,8 @@ aerodynamicCoef = 0.12 #tf is this?
 airAtSeaLevel = 1.225 #density of air at sea level in Kg/m^3
 gravity = 9.81 #m/s^2, acceleration of gravity
 velocityArray = [] #empty array for later
-drag = 0.5 * aerodynamicCoef * airAtSeaLevel * area #all-inclusive coeffecient?????????????????????????????????????????
+densityOfLiquid = 13351 #kg/m^3
+drag = 0.5 * aerodynamicCoef * densityOfLiquid * area #all-inclusive coeffecient?????????????????????????????????????????
 timeStep = 0.1 #interval of time
 totalDuration = 50 #seconds
 
@@ -30,7 +31,6 @@ mass = densityOfSphere / volume
 #mercury!!!!!!!!!!!!!
 #source: https://www.engineeringtoolbox.com/mercury-d_1002.html
 #temp 100 degrees Celcius
-densityOfLiquid = 13351 #kg/m^3
 
 
 #use this info to calculate buoyancy
@@ -51,7 +51,7 @@ accelerationArray = []
 
 #make calculations
 for i in range(2, len(timeIntervals)):
-    acceleration = gravity - (buoyancy / mass) * velocityArray[i -1] ** 2
+    acceleration = gravity - ((drag + buoyancy) / mass) * velocityArray[i -1] ** 2
     accelerationArray.append(acceleration)
     velocityArray.append(velocityArray[i -1] + acceleration * timeStep)
 
